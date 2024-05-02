c8c52a8a7ffef066afbd0e19392ada4f158fda2f
Federico Martin <federicoomartinok@gmail.com>
2024-05-02 18:49:56 -0300

    Cositas

---
+++ Clase03/Ejercicios/Ejercicio_20.php
@@ -0,0 +1,12 @@
+<?php
+// Aplicación No 20 BIS (Registro CSV)
+// Archivo: registro.php
+// método:POST
+// Recibe los datos del usuario(nombre, clave,mail )por POST ,
+// crear un objeto y utilizar sus métodos para poder hacer el alta,
+// guardando los datos en usuarios.csv.
+// retorna si se pudo agregar o no.
+// Cada usuario se agrega en un renglón diferente al anterior.
+// Hacer los métodos necesarios en la clase usuario
+
+//https://www.onlinegdb.com/5KfKRT1dXk ultimo gdb
